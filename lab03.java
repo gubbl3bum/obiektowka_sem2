893a77efbc337059305e0321eb42189651fb01f6
gubblebum <gubblebum@mail.com>
2023-03-26 23:34:40 +0200

    exercise update 1

--- lab03.java
+++ lab03.java
@@ -9,8 +9,11 @@ public class lab03{
         Scanner input = new Scanner(System.in);
         return input.nextInt();
     }
+    /*Grupa laboratoryjna składa się z n studentów (wartość n podaje użytkownik). Wprowadzamy
+    liczbę punktów dla każdego studenta. Napisz program, który obliczy średnią liczbę punktów w
+    grupie z wykorzystaniem pętli while*/
     public static void punkty() {
-        System.out.println("---------- ZADANIE 1 ----------");
+        System.out.println("\n---------- ZADANIE 1 ----------");
         System.out.print("podaj liczbe studentow: ");
         int l = inputInt();
         float wynik=0;
@@ -23,9 +26,10 @@ public class lab03{
         System.out.println(" ");
         System.out.format("srednia liczba punktow w grupie wynosi: %.2f ",wynik);
     }
-    public static void sumaUj() {
-        System.out.println(" ");
-        System.out.println("---------- ZADANIE 2 ----------");
+    /*. Napisz program, który pozwoli na wczytanie 10 liczb i wyznaczeniu ilości oraz sumy liczb
+    ujemnych i dodatnich podanych przez użytkownika*/
+    public static void sumaUj(){
+        System.out.println("\n---------- ZADANIE 2 ----------");
         int sumamin = 0, sumapl=0, iloscmin = 0, iloscpl=0;
         for(int i=0; i<10; i++){
             System.out.format("podaj %d liczbe: ", i+1);
@@ -40,8 +44,10 @@ public class lab03{
         }
         System.out.format("liczby ujemne: suma -> %d ilosc -> %d liczby dodatnie: suma -> %d ilosc -> %d",sumamin,iloscmin,sumapl,iloscpl);
     }
+    /*  Dany jest ciąg n liczb (n>0), napisz program pozwalający na obliczenie sumy wszystkich liczb
+    parzystych w danym ciągu */
     public static void ciag(){
-        System.out.println("---------- ZADANIE 3 ----------");
+        System.out.println("\n---------- ZADANIE 3 ----------");
         System.out.println("podaj ilosc liczb w ciagu: ");
         int k = inputInt();
         float suma = 0;
@@ -52,4 +58,11 @@ public class lab03{
         }
         System.out.format("suma liczb parzystych tego ciagu wynosi %.2f",suma);
     }
+    /*  Wykorzystując algorytm z zadania 5, napisz program który pozwoli użytkownikowi na
+    wylosowanie n liczb z przedziału (-10,45) i obliczy sumę wszystkich liczb parzystych w danym
+    ciągu. */
+
+    /* Napisz program, który odpowie na pytanie, czy podane przez użytkownika słowo jest
+    palindromem. Palindrom to słowo, które jest takie samo czytane od początku i od końca, np.
+    kajak.*/
 }
