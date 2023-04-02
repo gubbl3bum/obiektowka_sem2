bf342eedd2bd245fc1c0414c832ac0a03737fad1
gubblebum <gubblebum@mail.com>
2023-04-02 23:24:23 +0200

    lab 03 done!

--- lab04.java
+++ lab04.java
@@ -1,113 +1,105 @@
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 public class lab04 {
     public static void main(String[] args) {
-//zagadnienia();
+        sum();
+        writeArr();
+        stringArr();
+        str5words();
+        sortNum();
+        strong();
+        arrComp();
     }
-    public static void zagadnienia () {
-        System.out.println("---------- LAB 04 ----------");
-//int[] array; //->zadeklarowanie tablicy o nazwie array int
-//trzeba zaalokowac pamiec dla tablicy
-//arrayInt = new int[10];
-        int[] array = new int[10];
-        int[] array1 = new int[]{1, 2, 3, 4, 5, 6, 3}; //nie trzeba wpisywac wartosci rozmiaru, program przy kompilowaniu czyta jako tablica 6 elementowa
-//tablica stringow
-//String arrayString = new String[]{"ala"," ma"," kota"};
-//---------------------------------------inicjalizacja elementow tablicy----------------------------------------
-// array[0]=1;
-// array[1]=12;
-// array[2]=123;
-// array[3]=array[2]*array[1]; toporna deklaracja tablicy
-        final int arrLen = 10; //-> zmienna juz nie do ruszenia
-        int[] array2 = new int[arrLen];
-// for(int i=0; i
-// System.out.print(array1[i]+" ");
-// }
-//------------------------------sprawdzenie dlugosci tablicy -> nazwa_tab.lenght--------------------------------
-//---------------------definicja iteratora ktory bierze kazdy element tablicy/struktury-------------------------
-        for (int i : array1) {
-            System.out.print(i + " ");
-        }
-        System.out.println(" ");
-        char[] arrayChar = new char[]{'j', 'a', 'v', 'a'}; //tablica znakow
-        String tablicaString = new String(arrayChar);//przeksztalcenie tablicy znakow na string!
-        System.out.println(tablicaString);
-        String[] arrayString = new String[]{"jeden ", "dwa plus jeden", "trzy"};
-        System.out.println("dlugosc tablicy: " + arrayString.length); //dlugosc tablicy/pojemnika
-        System.out.println("dlugosc slowa: " + tablicaString.length()); //dlugosc pojemnika/sprawdzenie ile elementow ma napis
-//-----------------------------------------tablice wielowymiarowe-----------------------------------------------
-        int[][] matrixB = new int[3][4];
-        int[][][] data = new int[2][3][1];
-        int[][] matrixA = {
-                {1, 2, 3}, //wartosci 0 indeksu
-                {2, 3}, //wartosci 1 indeksu
-                {2, 3, 4, 5, 6, 7, 8, 5, 3},
-                {1},
-        }; //inicjalizacja tablicy wielowymiarowej z wartosciami
-//tablice moga miec rozne dlugosci!!!
-        for(int i = 0; i< matrixA.length; i++){
-            for (int j = 0; j< matrixA[i].length; j++){
-                System.out.print(matrixA[i][j] + " ");
-            }
-            System.out.println();
-        } System.out.println();
-        for(int[] innerArray : matrixA){
-            for(int data1 : innerArray){
-                System.out.print(data1+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-//drukowanie tablicy 3-wymiarowej
-        for(int[][] inner2DrArray : data){
-            for(int[] data1 : inner2DrArray){
-                for(int item: data1){
-                    System.out.println(item+" ");
-                }
-            }
-            System.out.println();
-        }
-//-------------------------------------------- KLASA ARRAYS ------------------------------------------------
-        int[] data1 = {1,2,3,45};
-        System.out.println(Arrays.toString(data1)); //tablica przekonwertowana na stringa -> bierze każdy poszczególny element i przerzuca na stringa
-        System.out.println();
-//ppm -> go to -> declaration or usages //ctrl + b
-        int[] data2 = new int[10];
-        Arrays.fill(data2,2); //automatyczne wypelnienie jedna wartoscia
-        System.out.println(Arrays.toString(data2));
-        int[] data3 = {1,2,3};
-        int[] data4 = {1,3,3};
-        System.out.println("wynik equals: "+Arrays.equals(data3,data4)); //zwraca true/false -> czy tablice są rowne
-        System.out.println("wynik compare: "+Arrays.compare(data3,data4)); //definiuje czy data3 jest rowny data4: rozne -> zwraca -1 te same -> zwraca 0
-        System.out.println();
-//----------------------------------------------------------------------------------------------------------
-        String tekst = "to jest tekst do podzialu na tablice";
-        String[] tekst_array= tekst.split(" "); //dzieli na pojemnik po regex(regular expression) -> tu po spacji
-// wyjdzie 7 elementow w tablicy
-        String tekscior = "to jest";
-        String[] tekst_arr= tekscior.split(" ");
-        String[] copyTekst_Array = new String[2];
-        System.arraycopy(tekst_arr,0,copyTekst_Array,0,tekst_arr.length);
-        String[] tab1 = Arrays.copyOfRange(tekst_arr,0,tekst_arr.length);
-        char[] charArr = tekst.toCharArray();
-        System.out.println(charArr);
+    public static int inputInt(){
+        Scanner input  = new Scanner(System.in);
+        return input.nextInt();
     }
-/*Napisz program obliczający sumę i średnią elementów tablicy z użyciem pętli for oraz for each.
+    public static String inputString(){
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+    /*Napisz program obliczający sumę i średnią elementów tablicy z użyciem pętli for oraz for each.
 Elementy do tablicy mają zostać wylosowane z dowolnego przedziały. Należy zaproponować
 oddzielną metodę do losowania liczb.*/
+    public static Random randFun(){
+        Random rand = new Random();
+        return rand;
+    }
+    public static void sum(){
+        System.out.println("---------- EXERCISE 1 ----------\n");
+        System.out.print("input list lenght: ");
+        int n = inputInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            //zakresem sa liczby (0, n)
+            arr[i] = randFun().nextInt(n);
+        }
+        int sum = 0;
+        for(int i : arr){
+            sum = sum + arr[i];
+        }
+        int ar_sum = sum/n;
+        System.out.println("sum: " + sum + "\narithmetic sum: " + ar_sum);
+        System.out.println("\narray: ");
+        for(int i : arr){
+            System.out.println(arr[i]);
+        }
 
+    }
 /*2. Napisz program, który wypisze co drugi element zdefiniowanych przez Ciebie tablic. Pierwsza
 tablica powinna mieć parzystą liczbę elementów, a druga nieparzystą.*/
-
+    public static void writeArr(){
+        System.out.println("\n---------- EXERCISE 2 ----------");
+        int[] arrEv = new int[20];
+        int[] arrOdd = new int[17];
+        for(int i = 0; i < arrEv.length; i++){
+            arrEv[i] = randFun().nextInt(100)+5;
+        }
+        System.out.println("Even numbers array: ");
+        for(int i = 0; i < arrEv.length/2; i++){
+            System.out.println((2*i) + " " + arrEv[2*i]);
+        }
+        for(int i = 0; i < arrOdd.length; i++){
+            arrOdd[i] = randFun().nextInt(115)-15;
+        }
+        System.out.println("\nOdd numbers array: ");
+        for(int i = 0; i < arrOdd.length/2 + 1; i++){
+            System.out.println((2*i) + " " + arrOdd[2*i]);
+        }
+    }
 /*3. Napisz program, w którym zdefiniujesz tablicę wartości typu String i zainicjalizujesz ją
 przykładowymi wartościami. Skorzystaj z pętli for-each, aby wypisać wszystkie wartości
 tablicy ze wszystkimi literami zamienionymi na wielkie. Skorzystaj z funkcjonalności
 toUpperCase wartości typu String..*/
-
+    public static void stringArr(){
+        System.out.println("\n---------- EXERCISE 3 ---------");
+        String[] strArr = new String[]{"example","text","to","upper","case"};
+        int j = 0;
+        for(String i : strArr){
+            System.out.println(strArr[j].toUpperCase());
+            j++;
+        }
+    }
 /*4. Napisz program, który pobierze od użytkownika pięć słów i zapisze je w tablicy. Następnie,
 program powinien wypisać wszystkie słowa, od ostatniego do pierwszego, z literami
 zapisanymi od końca do początku. Dla przykładu, dla podanych słów "Ala", "ma", "kota", "i",
 "psa" program powinien wypisać: "asp", "i", "atok", "am", "alA".*/
-
+    public static void str5words(){
+        System.out.println("\n---------- EXERCISE 4 ----------");
+        String[] strArr = new String[5];
+        for(int i = 0; i < strArr.length; i++){
+            System.out.print("give " + (i+1) + " word: ");
+            strArr[i] = inputString();
+        }
+        for(int i = (strArr.length - 1); i >= 0; i--){
+            char[] charArr = strArr[i].toCharArray();
+            for(int j = (charArr.length - 1); j >= 0; j--){
+                System.out.print(charArr[j]);
+            }
+            System.out.print(" ");
+        }
+    }
 /*5. Napisz program, który pobierze od użytkownika osiem liczb, zapisze je w tablicy, a następnie
 posortuje tą tablicę rosnąco i wypisze wynik sortowania na ekran. Dla przykładu, dla liczb 10,
 -2, 1, 100, 20, -15, 0, 10, program wypisze -15, -2, 0, 1, 10, 10, 20, 100. Zastanów się, jak można
@@ -115,12 +107,62 @@ posortować ciąg liczb i spróbuj zaimplementować swoje rozwiązanie. Przetest
 zestawach danych. Możesz też skorzystać z jednego z popularnych algorytmów sortowania, np.
 sortowania przez wstawianie. Działanie algorytmu omówiono szczegółowo na zajęciach
 algorytmy i struktury danych.*/
-
+    public static void sortNum(){
+        System.out.println("\n\n---------- EXERCISE 5 ----------\n");
+        final int arrLen = 8;
+        int[] arr = new int[arrLen];
+        for(int i = 0; i < arrLen; i++){
+            System.out.print("give " + (1+i) + " number: ");
+            arr[i] = inputInt();
+        }
+        //sortowanie bąbelkowe -> parami
+        System.out.println("correct order: \n");
+        for(int i = 0; i < arrLen - 1; i++){
+            for(int j = 0; j < arrLen - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        //Arrays.sort(arr); // -> sortowanie funkcją - najprosciej bez petli, funkcja wbudowana;
+        for(int i = 0; i < arrLen; i++) System.out.println(arr[i]);
+    }
 /*6. Napisz program, który pobierze od użytkownika pięć liczb, zapisze je w tablicy, a następnie
 policzy i wypisze silnię każdej z pobranych liczb.*/
-
+    public static void strong(){
+        System.out.println("\n---------- EXERCISE 6 ----------\n");
+        int arrLen = 5;
+        float[] arr = new float[arrLen];
+        for(int i = 0; i < arrLen; i++){
+            System.out.print("give " + (i+1) + " number: ");
+            arr[i] = lab02.inputFloat();
+        }
+        System.out.println();
+        float res;
+        for(int i = 0; i < arrLen; i++){
+            res = strFun(arr[i]);
+            System.out.print("result: " + res + " \n");
+        }
+    }
+    public static float strFun(float f) { // deklaracja metody do znajdowania silni rekurencyjnie w Javie
+        if (f <= 1) { return 1;}
+        else { return f * strFun(f - 1);  }
+    }
 /*7. Napisz program, w którym zdefiniujesz dwie tablice przechowujące wartości typu String.
 Zainicjalizuj obie tablice takimi samymi wartościami, w takiej samej kolejności. Napisz kod,
 który porówna obie tablice i odpowie na pytanie, czy są one takie same*/
-
+    public static void arrComp(){
+        System.out.println("\n----------- EXERCISE 7 ----------\n");
+        String[] arr1 = new String[]{"1","2","3","4","5","15"};
+        String[] arr2 = new String[]{"1","2","3","4","5","15"};
+        //uzycie klasy Arrays z .equals -> zwraca true/false! compare zwraca wartosc 1/0
+        //System.out.println("is arr1 equals to arr2: " + Arrays.equals(arr1,arr2));
+        boolean res = false;
+        for(int i = 0; i < arr1.length; i++){
+            if(arr1[i] == arr2[i]) res = true;
+        }
+        System.out.println("is arr1 equals to arr2: " + res);
+    }
 }
---
+++ lab04_notes.java
@@ -0,0 +1,94 @@
+import java.util.Arrays;
+public class lab04_notes {
+    public static void main(String[] args) {
+//zagadnienia();
+    }
+    public static void zagadnienia () {
+        System.out.println("---------- LAB 04 ----------");
+//int[] array; //->zadeklarowanie tablicy o nazwie array int
+//trzeba zaalokowac pamiec dla tablicy
+//arrayInt = new int[10];
+        int[] array = new int[10];
+        int[] array1 = new int[]{1, 2, 3, 4, 5, 6, 3}; //nie trzeba wpisywac wartosci rozmiaru, program przy kompilowaniu czyta jako tablica 6 elementowa
+//tablica stringow
+//String arrayString = new String[]{"ala"," ma"," kota"};
+//---------------------------------------inicjalizacja elementow tablicy----------------------------------------
+// array[0]=1;
+// array[1]=12;
+// array[2]=123;
+// array[3]=array[2]*array[1]; toporna deklaracja tablicy
+        final int arrLen = 10; //-> zmienna juz nie do ruszenia
+        int[] array2 = new int[arrLen];
+// for(int i=0; i
+// System.out.print(array1[i]+" ");
+// }
+//------------------------------sprawdzenie dlugosci tablicy -> nazwa_tab.lenght--------------------------------
+//---------------------definicja iteratora ktory bierze kazdy element tablicy/struktury-------------------------
+        for (int i : array1) {
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        char[] arrayChar = new char[]{'j', 'a', 'v', 'a'}; //tablica znakow
+        String tablicaString = new String(arrayChar);//przeksztalcenie tablicy znakow na string!
+        System.out.println(tablicaString);
+        String[] arrayString = new String[]{"jeden ", "dwa plus jeden", "trzy"};
+        System.out.println("dlugosc tablicy: " + arrayString.length); //dlugosc tablicy/pojemnika
+        System.out.println("dlugosc slowa: " + tablicaString.length()); //dlugosc pojemnika/sprawdzenie ile elementow ma napis
+//-----------------------------------------tablice wielowymiarowe-----------------------------------------------
+        int[][] matrixB = new int[3][4];
+        int[][][] data = new int[2][3][1];
+        int[][] matrixA = {
+                {1, 2, 3}, //wartosci 0 indeksu
+                {2, 3}, //wartosci 1 indeksu
+                {2, 3, 4, 5, 6, 7, 8, 5, 3},
+                {1},
+        }; //inicjalizacja tablicy wielowymiarowej z wartosciami
+//tablice moga miec rozne dlugosci!!!
+        for(int i = 0; i< matrixA.length; i++){
+            for (int j = 0; j< matrixA[i].length; j++){
+                System.out.print(matrixA[i][j] + " ");
+            }
+            System.out.println();
+        } System.out.println();
+        for(int[] innerArray : matrixA){
+            for(int data1 : innerArray){
+                System.out.print(data1+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+//drukowanie tablicy 3-wymiarowej
+        for(int[][] inner2DrArray : data){
+            for(int[] data1 : inner2DrArray){
+                for(int item: data1){
+                    System.out.println(item+" ");
+                }
+            }
+            System.out.println();
+        }
+//-------------------------------------------- KLASA ARRAYS ------------------------------------------------
+        int[] data1 = {1,2,3,45};
+        System.out.println(Arrays.toString(data1)); //tablica przekonwertowana na stringa -> bierze każdy poszczególny element i przerzuca na stringa
+        System.out.println();
+//ppm -> go to -> declaration or usages //ctrl + b
+        int[] data2 = new int[10];
+        Arrays.fill(data2,2); //automatyczne wypelnienie jedna wartoscia
+        System.out.println(Arrays.toString(data2));
+        int[] data3 = {1,2,3};
+        int[] data4 = {1,3,3};
+        System.out.println("wynik equals: "+Arrays.equals(data3,data4)); //zwraca true/false -> czy tablice są rowne
+        System.out.println("wynik compare: "+Arrays.compare(data3,data4)); //definiuje czy data3 jest rowny data4: rozne -> zwraca -1 te same -> zwraca 0
+        System.out.println();
+//----------------------------------------------------------------------------------------------------------
+        String tekst = "to jest tekst do podzialu na tablice";
+        String[] tekst_array= tekst.split(" "); //dzieli na pojemnik po regex(regular expression) -> tu po spacji
+// wyjdzie 7 elementow w tablicy
+        String tekscior = "to jest";
+        String[] tekst_arr= tekscior.split(" ");
+        String[] copyTekst_Array = new String[2];
+        System.arraycopy(tekst_arr,0,copyTekst_Array,0,tekst_arr.length);
+        String[] tab1 = Arrays.copyOfRange(tekst_arr,0,tekst_arr.length);
+        char[] charArr = tekst.toCharArray();
+        System.out.println(charArr);
+    }
+}
